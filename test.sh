76fc63916dd72520ce4a6f8529958d0a184e08a3
fiziechemi <fiziechemi@gmail.com>
2023-07-18 12:34:05 +0000

    update

--- test.sh
+++ test.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# if total argument not equal to 1, echo "error"
+if $# != 1; then
+  echo "Error: wrong arguments"
+fi
+
 # Create a vnc display if it doesn't exist
 if ! pgrep -f "x11vnc -create -env FD_PROG=/usr/bin/fluxbox -env X11VNC_FINDDISPLAY_ALWAYS_FAILS=1 -env X11VNC_CREATE_GEOM=1024x768x16 -gone killall Xvfb -bg -nopw" > /dev/null; then
     nohup x11vnc -create \
@@ -15,46 +20,12 @@ fi
 #     pkill -f "x11vnc -create -env FD_PROG=/usr/bin/fluxbox -env X11VNC_FINDDISPLAY_ALWAYS_FAILS=1 -env X11VNC_CREATE_GEOM=1024x768x16 -gone killall Xvfb -bg -nopw"
 # fi
 
-# # Find created virtual display number
-# DISPLAY_FOUND=false
-# DISPLAY_NUMBER=0
-# while true; do
-#   if xdpyinfo -display :$DISPLAY_NUMBER >/dev/null 2>&1; then
-#     DISPLAY_FOUND=true
-#     break
-#   fi
-#   DISPLAY_NUMBER=$((DISPLAY_NUMBER + 1))
-#   if [ $DISPLAY_NUMBER -gt 1023 ]; then
-#     break
-#   fi
-# done
-
-# # Set display number
-# if $DISPLAY_FOUND; then
-#   export DISPLAY=:${DISPLAY_NUMBER}
-# else
-#   echo "Virtual display not found"
-#   exit 1
-# fi
-
-
-
-
-
-
-# Find created virtual display number
-DISPLAY_NUMBER=0
-while true; do
-  DISPLAY_NUMBER=$((DISPLAY_NUMBER + 1))
-  xdpyinfo -display :$DISPLAY_NUMBER
-#   if xdpyinfo -display :$DISPLAY_NUMBER >/dev/null 2>&1; then
-#     echo "$DISPLAY_NUMBER" > /electron/display
-#     break
-#   fi
-  if [ $DISPLAY_NUMBER -gt 1023 ]; then
-    if [ -f /electron/display ]; then
-      rm /electron/display
-    fi
-    break
-  fi
-done
+FINDDISPLAY=$(x11vnc --finddpy)
+if echo "$FINDDISPLAY" | grep -q "^DISPLAY=:"; then
+    value=${FINDDISPLAY#*=}
+    value=${value%%,*}
+    export DISPLAY=$value
+else
+    echo "Error: Virtual display not found"
+    exit 1
+fi
